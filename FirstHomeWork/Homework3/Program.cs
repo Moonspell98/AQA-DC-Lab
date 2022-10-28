09695249b03c4de354b4d2abc97ddb4f292266ba
Siarhei.Hlitsuk <moonspel98@gmail.com>
2022-10-28 12:04:19 +0200

    Task 2 added

---
+++ FirstHomeWork/Homework3/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Task2();
+
+static void Task1()
+{
+    Console.WriteLine("Enter number: ");
+    var inputNumber = int.Parse(Console.ReadLine());
+    var result = 0;
+    for (int i = 0; i <= inputNumber; i++)
+    {
+        result += i;
+    }
+    Console.WriteLine($"The sum of numbers between 0 and {inputNumber} is: {result}");
+}
+
+static void Task2()
+{
+    var multiplicationTableForNumber = 3;
+    var i = 0;
+    while (i <= 10)
+    {
+        Console.WriteLine($"{multiplicationTableForNumber} * {i} = {multiplicationTableForNumber*i}");
+        i++;
+    }
+}

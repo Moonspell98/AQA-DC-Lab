ec522d6b7c292123afbd70e59956ab20f9a8808e
Siarhei.Hlitsuk <moonspel98@gmail.com>
2022-11-14 19:09:06 +0100

    First Task finished

---
+++ FirstHomeWork/Homework6/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Homework6;
+
+Car car = new Car("BMW X5", "Automatic", 3000, 50000);
+Motorcicle motorcicle = new Motorcicle("Yamaha Drag Star", true, 400, 10000);
+
+car.Ride(10);
+motorcicle.Ride(3);
+
+car.ShowInfo();
+motorcicle.ShowInfo();
+
+
---
+++ FirstHomeWork/Homework6/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6
+{
+    public abstract class Vehicle
+    {
+        public int mass;
+
+        public string ModelName { get; set; }
+        public abstract double Cost { get; set; }
+
+        public abstract void Ride(int rideLong);
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"{ModelName} mass is: {mass}\n");
+        }
+    }
+}

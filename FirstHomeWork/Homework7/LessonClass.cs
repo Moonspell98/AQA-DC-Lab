203ae00bb5ef8abe3d59b46ace037c5ed64113d4
Siarhei.Hlitsuk <moonspel98@gmail.com>
2022-11-16 19:01:45 +0100

    Task 1 added

---
+++ FirstHomeWork/Homework7/LessonClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7
+{
+    internal class LessonClass
+    {
+        public static void ShowMassiveElement()
+        {
+            int[] massive = { 8, 7, 1, 4, 2 };
+
+            Console.WriteLine("Input index of element in massive:");
+            string? inputedValue = null;
+
+            try
+            {
+                string? checkedValue = inputedValue.Equals(string.Empty) ? null : inputedValue;
+                int inputtedNumber = Int32.Parse(checkedValue);
+                int massiveElement = massive[inputtedNumber];
+                Console.WriteLine($"Massive element that has index {inputedValue} has value {massiveElement}");
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("I don't how how you did it but you entered \"null\" value in Console.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid index format (not a number)");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Entered index is not fits in current array!");
+            }
+            finally
+            {
+                Console.WriteLine("Program finished whether it's correct or not");
+            }
+        }
+
+    }
+}

54705ba5533006217931c9294d90ef1050d4fb29
Siarhei.Hlitsuk <moonspel98@gmail.com>
2022-11-17 18:00:29 +0100

    Dictionary task 3 added

---
+++ FirstHomeWork/Homework8/Dictionary/DictionaryTask3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8.Dictionary
+{
+    public class City
+    {
+        public int population;
+        public double sqaure;
+        public City(int population, double sqare)
+        {
+            this.population = population;
+            this.sqaure = sqare;
+        }
+        public override string ToString() => $"Population: {population}, square: {sqaure}";
+
+    }
+
+
+}
--- FirstHomeWork/Homework8/Program.cs
+++ FirstHomeWork/Homework8/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Homework8.Dictionary;
 using Homework8.LinkedList;
 using Homework8.List;
 // List Task 1/2
@@ -11,12 +12,47 @@ using Homework8.List;
 // Using refactored method
 //ListTask2.WriteStringsAsUserWants(listOfStrings);
 
-var employees = new List<int> { 1, 2, 3 };
-LinkedList<int> people = new LinkedList<int>(employees);
+// Linked List Task 1
+//var justList = new List<int> { 1, 2, 3 };
+//LinkedList<int> linkedList = new LinkedList<int>(justList);
 
-LinkedListTask1.InsetElements(1, 2, people);
+//LinkedListTask1.InsetElements(1, 2, linkedList);
 
-foreach(var employee in people)
+//foreach(var employee in linkedList)
+//{
+//    Console.WriteLine(employee);
+//}
+
+// Dictionary Task 3
+
+var cities = new Dictionary<string, City>
 {
-    Console.WriteLine(employee);
+    { "Minsk", new City(20, 200) },
+    { "Warsaw", new City(21, 210) },
+    { "Wroclaw", new City(10, 120) },
+    { "Vilnus", new City(12, 100) },
+    { "Milan", new City(9, 60) }
+};
+
+// Sort by square
+var sortedCities = cities.OrderBy(_ => _.Value.sqaure).ToDictionary(_ => _.Key, _ => _.Value);
+PrintDictionary(sortedCities);
+
+// Sort by population desc
+
+sortedCities = cities.OrderByDescending(_=>_.Value.population).ToDictionary(_ => _.Key, _ => _.Value);
+PrintDictionary(sortedCities);
+
+// Overall population
+
+var overallPopulation = cities.Sum(_=>_.Value.population);
+Console.WriteLine($"Overall Population is {overallPopulation}");
+
+void PrintDictionary(Dictionary<string, City> dictionary)
+{
+    foreach (var item in dictionary)
+    {
+        Console.WriteLine($"Key is: {item.Key}, value {item.Value}");
+    }
+    Console.WriteLine();
 }

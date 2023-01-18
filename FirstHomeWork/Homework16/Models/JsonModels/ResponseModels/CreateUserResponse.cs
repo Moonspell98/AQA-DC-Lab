f42040fd20091d46ca2fddb5a8ae915e23e276e0
Siarhei.Hlitsuk <moonspel98@gmail.com>
2023-01-18 12:32:43 +0100

    Http client added along with tests

---
+++ FirstHomeWork/Homework16/Models/JsonModels/ResponseModels/CreateUserResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework16.Models.JsonModels.ResponseModels
+{
+    public class CreateUserResponse
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}

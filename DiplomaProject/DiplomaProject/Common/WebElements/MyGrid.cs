e4838dd034525c3a191cf783b5fb9cbcc5cd9fd7
Siarhei.Hlitsuk <moonspel98@gmail.com>
2023-01-24 10:14:21 +0100

    Changes applied

--- DiplomaProject/DiplomaProject/Common/WebElements/MyGrid.cs
+++ DiplomaProject/DiplomaProject/Common/WebElements/MyGrid.cs
@@ -3,9 +3,16 @@ using OpenQA.Selenium;
 
 namespace DiplomaProject.Common.WebElements
 {
-    public class MyGrid : MyWebElement
+    public class MyGrid
     {
-        public IWebElement FindRowByColumnValue(string columnName, string value) => FindElement(By.XPath($".//div[contains(@class, 'table-cell')][count(//*[@role='columnheader'][text()='{columnName}']//preceding-sibling::div) + 1]/div[text()='{value}']/ancestor::div[@role='row']"));
+        private MyWebElement Grid;
+
+        public MyGrid(By by)
+        {
+            Grid = new MyWebElement(by);
+        }
+
+        public IWebElement FindRowByColumnValue(string columnName, string value) => Grid.FindElement(By.XPath($".//div[contains(@class, 'table-cell')][count(//*[@role='columnheader'][text()='{columnName}']//preceding-sibling::div) + 1]/div[text()='{value}']/ancestor::div[@role='row']"));
 
         public string GetCellTextByColumn(string searchColumnName, string searchValue, string columnName)
         {
@@ -34,10 +41,6 @@ namespace DiplomaProject.Common.WebElements
             var row = FindRowByColumnValue(searchColumnName, searchValue);
             var selectCheckbox = row.FindElement(By.XPath(".//*[contains(@class, 'oxd-checkbox-input')]"));
             selectCheckbox.Click();
-        }
-
-        public MyGrid(By by) : base(by)
-        {
         }
     }
 }
